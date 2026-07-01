6dd557abf2333daafa05003a41c36acca53c6857
ruth-chirwa <chirwaruth647@gmail.com>
2026-07-01 10:43:40 +0200

    add db connction

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "safewatch",
-  password: "safewatch",
-  database: "safewatch"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
